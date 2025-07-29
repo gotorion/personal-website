2b784d5989c138ce2deef2ba9d75a7ee6b7edc6b
Junhui Li <junhui.lee@icloud.com>
2025-07-29 21:58:43 +0800

    Remove about nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,6 @@ const Header = () => {
 
   const navItems = [
     { name: 'Home', href: '#home' },
-    { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
     { name: 'Contact', href: '#contact' },
